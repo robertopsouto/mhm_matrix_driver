fd7270770735b0a88bd6ccef56517cb91fc11a83
Roberto P Souto <rpsouto@lncc.br>
2017-03-02 11:03:10 -0300

    Mudanças a serem submetidas: 	new file:   scripts/view.py

---
+++ scripts/view.py
@@ -0,0 +1,8 @@
+from scipy.io import mminfo,mmread
+A=mmread('matrix.mtx')
+import matplotlib.pyplot as plt
+plt.spy(A,markersize=1)
+from matplotlib.pyplot import figure, show
+import numpy
+show()
+
