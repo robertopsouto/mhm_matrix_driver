bf26c89b6ed224f06c9b377893e04b1d16e2d13a
Roberto P Souto <rpsouto@lncc.br>
2017-03-02 11:33:22 -0300

    new file:   MatrixMarket/MHM/6656/figureraster.py 	new file:   scripts/figureraster.py 	deleted:    scripts/view.py

---
+++ scripts/figureraster.py
@@ -0,0 +1,7 @@
+from scipy.io import mminfo,mmread
+A=mmread('MatrixMarket_MHM_subproblem.mtx')
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.spy(A,markersize=1)
+plt.savefig('MatrixMarket_MHM_subproblem')
--- scripts/view.py
+++
@@ -1,8 +0,0 @@
-from scipy.io import mminfo,mmread
-A=mmread('matrix.mtx')
-import matplotlib.pyplot as plt
-plt.spy(A,markersize=1)
-from matplotlib.pyplot import figure, show
-import numpy
-show()
-
