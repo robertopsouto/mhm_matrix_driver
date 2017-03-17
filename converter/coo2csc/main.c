97eefb04ae68297e2837327c512bc4012fd9704f
rpsouto <rpsouto@lncc.br>
2017-03-17 05:21:23 -0300

    converter new

--- converter/coo2csc/main.c
+++ converter/coo2csc/main.c
@@ -1,12 +1,8 @@
+#include <bebop/smc/csc_matrix.h>
+
 #include <bebop/util/config.h>
 #include <bebop/smc/sparse_matrix.h>
 #include <bebop/smc/sparse_matrix_ops.h>
-#include <bebop/smc/coo_matrix.h>
-#include <bebop/smc/coord_elem.h>
-#include <bebop/smc/csc_matrix.h>
-#include <bebop/smc/csr_matrix.h>
-#include <bebop/smc/mmio.h>
-#include <bebop/smc/read_mm.h>
 
 #include <bebop/util/get_options.h>
 #include <bebop/util/init.h>
@@ -14,38 +10,82 @@
 #include <bebop/util/malloc.h>
 #include <bebop/util/timer.h>
 #include <bebop/util/util.h>
-#include <bebop/util/complex.h>
-#include <bebop/util/merge_sort.h>
-#include <bebop/util/log.h>
 
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
+int  
+matrix_properties(const struct csc_matrix_t* A);
+
 int main(int argc, char *argv[])
 {
 
+    enum sparse_matrix_file_format_t informat = 0;
     const char *filename;
-    int code=1;
+    double seconds = 0.0;
+    int errcode = 0;
+    enum sparse_matrix_storage_format_t output_format;
 
     if (argc < 2)
         {
                 fprintf(stderr, "Usage: %s [matrix-market-input-filename]\n", argv[0]);
-                exit(1);
+                return 1;
         }
     filename = argv[1];
     printf("filename: %s \n",filename);
-    struct coo_matrix_t* A = bebop_malloc (sizeof (struct coo_matrix_t));
-    //code = read_matrix_market_real_sparse (filename, A);
-    code = read_matrix_market_sparse (filename, A);
-    if (code != 0)
-       {
-             fprintf(stderr, "Could not open Matrix Market file");
-             exit(1);
-       }
-    printf("read code: %d \n",code);
+    struct sparse_matrix_t* A = NULL;
+
+    printf ("Loading sparse matrix...");
+    informat = MATRIX_MARKET;
+    seconds = get_seconds();
+    A = load_sparse_matrix (informat, filename);
+    seconds = get_seconds() - seconds;
+    if (A == NULL)
+      {
+        fprintf (stderr, "*** Failed to load input matrix file \"%s\" ***\n",
+               filename);
+        return 1;
+      }
+    printf ("done, in %g seconds\n", seconds);
+
+    printf ("Validating sparse matrix...");
+    seconds = get_seconds();
+    errcode = valid_sparse_matrix (A);
+    seconds = get_seconds() - seconds;
+    printf ("done, in %g seconds\n", seconds);
+
+    if (valid_sparse_matrix (A))
+      printf ("\n### Sparse matrix is valid ###\n\n");
+    else
+      {
+        printf ("\n### Invalid sparse matrix! ###\n\n");
+        return 1;
+      }
+    
+    output_format = CSC;
+    printf ("Converting COO to CSC format...");
+    seconds = get_seconds();
+    errcode = sparse_matrix_convert (A, output_format);
+    if ( errcode !=0 )
+      {
+        fprintf (stderr, "*** Failed to convert COO to CSC format...");
+        return 1;
+      }      
+    seconds = get_seconds() - seconds;
+    printf ("done, in %g seconds\n", seconds);
+
+    matrix_properties(A->repr);
 
     return 0;
+}
+
+
+int 
+matrix_properties(const struct csc_matrix_t* A)
+{
+   printf("\n m, n, nnz: %d %d %d \n", A->m, A->n, A->nnz);
+   return 0;
 }
 
